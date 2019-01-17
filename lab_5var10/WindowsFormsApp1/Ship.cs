9f7124a35ec01eaed2149f3018f696396974274e
Dmitry KLinyshev <klinyshev.dmitrii@mail.ru>
2019-01-17 23:07:30 +0400

    final

---
+++ lab_5var10/WindowsFormsApp1/Ship.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormHarbour;
+
+
+
+namespace WindowsFormsApp1
+{
+    abstract class Ship : ITransport
+    {
+
+        protected float _startPosX;
+        protected float _startPosY;
+        protected float _pictureWidth;
+        protected float _pictureHeight;
+
+        public int MaxSpeed { protected set; get; }
+
+        public float Weight { protected set; get; }
+
+        public Color MainColor { protected set; get;}
+
+        public void setPosition(int x, int y, int width, int height)
+        {
+            _startPosX = x;
+            _startPosY = y;
+            _pictureWidth = width;
+            _pictureHeight = height;
+        }
+
+        public void setMainColor(Color color)
+        {
+            MainColor = color;
+        }
+
+        public abstract void DrawBoat(Graphics g);
+
+       
+        public abstract void moveTransport(Direction direction);
+    }
+}
